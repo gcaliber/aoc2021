dcbdbfc60ce9a2edf19bac3dff2e9904fbbcedcf
Michael Green <mgreen1980@gmail.com>
2021-12-13 17:57:51 -0500

    Completed 1

--- 01/main.py
+++ 01/main.py
@@ -1,5 +1,53 @@
-if __name__ == "__main__":
-    with open("C:\\Users\\mgreen\\OneDrive - Virginia Lottery\\code\\aoc\\01") as f:
-        input = [it for it in f.readline(input)]
+# file = "C:\\Users\\mgreen\\code\\aoc\\01\\sample.txt"
+file = "C:\\Users\\mgreen\\code\\aoc\\01\\input.txt"
+
+class Window(object):
+    def __init__(self, nums):
+        self.nums = nums
+        self.size = len(nums)
+        self.pos = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.pos + 2 < self.size:
+            i = self.pos
+            window = self.nums[i] + self.nums[i+1] + self.nums[i+2]
+            self.pos += 1
+            return window
+        raise StopIteration()
+
+def read_input(fn):
+    with open(fn) as f:
+        return [int(line.rstrip()) for line in f.readlines()]
+
+def part1():
+    input = read_input(file)
     
-    print(input)
+    total = 0
+    for i, it in enumerate(input):
+        if i != 0:
+            if it - input[i - 1] > 0:
+                total += 1
+    
+    print(total)
+
+def part2():
+    input = read_input(file)
+
+    w = Window(input)
+
+    total = 0
+    p = next(w)
+    for it in w:
+        if it - p > 0:
+            total += 1
+        p = it
+    
+    print(total)
+
+if __name__ == "__main__":
+    part1()
+    part2()
+    
