4251345e0efee970446356e45eb388ba6a2bb234
Michael Green <mgreen@LOCAL.VALOTTERY.COM>
2021-12-13 17:45:58 -0500

    First commit for AoC 2021

---
+++ 01/main.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    with open("C:\\Users\\mgreen\\OneDrive - Virginia Lottery\\code\\aoc\\01") as f:
+        input = [it for it in f.readline(input)]
+    
+    print(input)
